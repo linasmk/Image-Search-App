82605ae1e4e98fcb92d2e4f3c1bc16104bdb5a8c
linasmk <linas.mackonis@gmail.com>
2020-11-02 13:37:21 +0100

    process.env is server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
-// require("dotenv").config();
+require("dotenv").config();
 
 /* Environment variable that Heroku sets up for the application.
 If the application runs on a local machine then the application defaults to port 3000 */
