8bef308452ca56ba1a3844ef1d858e260509ce8e
linasmk <linas.mackonis@gmail.com>
2020-11-02 17:32:57 +0100

    Loading spinner added Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+/* ========= App Dependencies ============= */
+import React from "react";
+/* ========= Code ============= */
+const Loader = (props) => {
+  return (
+    <div className="loader-box">
+      <div className="loader"></div>;
+    </div>
+  );
+};
+
+export default Loader;
