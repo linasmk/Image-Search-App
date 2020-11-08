cdf04239f27b75abdbac2d9f65d227cac0fd431e
linasmk <linas.mackonis@gmail.com>
2020-11-08 19:43:50 +0100

    Testing Heroku config vars

--- accessKey.js
+++ accessKey.js
@@ -3,6 +3,7 @@ import Unsplash, { toJson } from "unsplash-js";
 /* ============================================
 =========== ACCESS KEY  ================
 ================================================= */
+const API_KEY = process.env.API_KEY;
 export const UnsplashAccessKey = new Unsplash({
-  accessKey: process.env.API_KEY,
+  accessKey: API_KEY,
 });
