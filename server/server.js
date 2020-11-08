3290523bd8ba66ad3daeef2c5754a6a6d10d598e
linasmk <linas.mackonis@gmail.com>
2020-11-08 23:35:39 +0100

    Testing Heroku API_KEY

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
 require("dotenv").config();
+console.log(process.env.API_KEY);
 
 /* Environment variable that Heroku sets up for the application.
 If the application runs on a local machine then the application defaults to port 3000 */
