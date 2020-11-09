fd350c086dd113000082a49002ab99347e677011
linasmk <linas.mackonis@gmail.com>
2020-11-09 01:06:09 +0100

    Reverting changes

--- accessKey.js
+++ accessKey.js
@@ -3,7 +3,6 @@ import Unsplash, { toJson } from "unsplash-js";
 /* ============================================
 =========== ACCESS KEY  ================
 ================================================= */
-
 export const UnsplashAccessKey = new Unsplash({
-  accessKey: process.env.API_KEY,
+  accessKey: "oLo0hESdVZbyaG0NaDBRZOotirJMglCd7hQXvMvAqRY",
 });
